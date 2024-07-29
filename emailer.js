95b185252b009e61552e277d5b92ed5aa81913cf
AkimT13 <akimtarasov@gmail.com>
2024-07-29 12:59:20 -0700

    mispell

--- emailer.js
+++ emailer.js
@@ -7,7 +7,7 @@ export async function sendEmail(receipientEmail, fName, subject, textString ) {
   
       //Authentication for our sending email.
       const transporter = nodemailer.createTransport({
-        host: "stmp.gmail.com",
+        host: "smtp.gmail.com",
         port: 465,
         secure: true,
         auth: {
