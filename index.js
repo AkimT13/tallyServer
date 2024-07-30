20d5087dc03281e53b5d934b3230e0431721ab8c
AkimT13 <akimtarasov@gmail.com>
2024-07-29 22:42:18 -0700

    trying this

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ import { sendEmail } from "./emailer.js";
 import bodyParser from "body-parser";
 
 const app = new express();
-app.use(express.json());
-app.use(bodyParser.json({limit:'200mb'}))
-app.use(bodyParser.urlencoded({ limit: '200mb', extended: true }));
+app.use(express.json({limit:'10mb'}));
+
 
 const formatData = (data) => {};
 
