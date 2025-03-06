5cc29039d52a90d4b3594b5e58122cdc32f5a3b0
AkimT13 <akimtarasov@gmail.com>
2025-03-06 09:39:08 -0800

    sendEmail returns boolean

--- emailer.js
+++ emailer.js
@@ -1,20 +1,5 @@
-import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-import fs from "fs";
-import path from "path";
-import Handlebars from "handlebars";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import { createPrivateKey } from "crypto";
-
-dotenv.config();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 export async function sendEmailHtml(recipientEmail, subject, templateName, data) {
   try {
-    // Authentication for our sending email
     const transporter = nodemailer.createTransport({
       host: "mail.privateemail.com",
       port: 465,
@@ -27,19 +12,15 @@ export async function sendEmailHtml(recipientEmail, subject, templateName, data)
         domainName: 'sfhacks.io',
         keySelector: 'default',
         privateKey: process.env.DKIM_PRIVATE_KEY
-
       },
-      
     });
 
     // Read the HTML template file from the project folder
-    const templatePath = path.join(".", `/templates/${templateName}.hbs`);
+    const templatePath = path.join(__dirname, `/templates/${templateName}.hbs`);
     const source = fs.readFileSync(templatePath, 'utf8');
 
     // Compile the Handlebars template
     const template = Handlebars.compile(source);
-
-    // Generate the final HTML with injected data
     const htmlToSend = template(data);
 
     // The message to be sent
@@ -50,34 +31,37 @@ export async function sendEmailHtml(recipientEmail, subject, templateName, data)
       },
       to: recipientEmail,
       subject: subject,
-      html: htmlToSend, // Injected HTML with user-specific data
-      attachments: [{
-        filename: 'sfhackslogo.png',
-        path: __dirname + '/templates/images/sfhackslogo.png',
-        cid: 'sfhackslogo'
-      },
-      {
-        filename: 'discord.png',
-        path: __dirname + '/templates/images/discord.png',
-        cid: 'discordlogo'
-      },
-      {
-        filename: 'instagram.png',
-        path: __dirname + '/templates/images/instagram.png',
-        cid: 'instagramlogo'
-      },
-      {
-        filename: 'solo.png',
-        path: __dirname + '/templates/images/solo.png',
-        cid: 'solologo'
-      }
+      html: htmlToSend,
+      attachments: [
+        {
+          filename: 'sfhackslogo.png',
+          path: path.join(__dirname, '/templates/images/sfhackslogo.png'),
+          cid: 'sfhackslogo'
+        },
+        {
+          filename: 'discord.png',
+          path: path.join(__dirname, '/templates/images/discord.png'),
+          cid: 'discordlogo'
+        },
+        {
+          filename: 'instagram.png',
+          path: path.join(__dirname, '/templates/images/instagram.png'),
+          cid: 'instagramlogo'
+        },
+        {
+          filename: 'solo.png',
+          path: path.join(__dirname, '/templates/images/solo.png'),
+          cid: 'solologo'
+        }
       ]
     };
 
     // Send the email
-    const info = await transporter.sendMail(mailOptions);
-    console.log("Email sent: " + info.response);
+    await transporter.sendMail(mailOptions);
+    console.log(`Email sent to ${recipientEmail}`);
+    return true;  
   } catch (err) {
-    console.error("Error sending email:", err);
+    console.error(`Error sending email to ${recipientEmail}:`, err);
+    return false;  
   }
 }
